4a4b49979065ba1fb4c0e50004a82aab1114dcbb
just-passion <tinycolorfulhummingbird@gmail.com>
2024-10-27 11:10:17 +0530

    api routes regex patterns

--- src/app.js
+++ src/app.js
@@ -10,6 +10,28 @@
    res.send("Data saved to the DB");//response to incoming req
 });
 
+app.get("/ab?c",(req, res) => { //b is optional
+   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
+});
+
+app.get("/ab+c",(req, res) => { //b can be 1 or more
+   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
+});
+
+app.get("/ab*c",(req, res) => { //anything can be between b and c
+   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
+});
+
+app.get("/a(bc)?d",(req, res) => { //bc is optional
+   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
+});
+
+app.get(/a/,(req, res) => { //if a is there anywhere
+   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
+});
+
+//can use regex patterns for routes
+
  app.listen(3000, ()=>{
     console.log("server is listening successfully");
     
