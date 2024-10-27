806bb70e534c997b6fe44c87334bda22361c2baf
just-passion <tinycolorfulhummingbird@gmail.com>
2024-10-27 11:55:49 +0530

    instead of 3rd argument for next, different method

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
-const express = require("express");
+ const express = require("express");
 
-const app = express(); //web app on server
+ const app = express(); //web app on server
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    //Route Handler
-    console.log("1st response");
-   //  res.send("1st Response");
-    next();
-  },
-  (req, res, next) => {
-    //Route Handler
-    console.log("2nd response");
-    res.send("2nd Response");
-    next();
-  }
-);
+ app.use("/user", (req, res, next) => {
+   //Route Handler
+   console.log("1st response");
+   // res.send("1st Response");  
+   next();
+ });
 
-app.listen(3000, () => {
-  console.log("server is listening successfully");
-}); //listen on that app
+ app.use("/user", (req, res, next) => {
+   //Route Handler
+   console.log("2nd response");
+   res.send("2nd Response");
+ });
+
+ app.listen(3000, ()=>{
+    console.log("server is listening successfully");
+    
+ }); //listen on that app
