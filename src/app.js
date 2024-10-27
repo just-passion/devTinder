79aebe03ca0d3120b315b6075eb05c0ee522015a
just-passion <tinycolorfulhummingbird@gmail.com>
2024-10-27 11:18:43 +0530

    exploring routing using params and query

--- src/app.js
+++ src/app.js
@@ -2,35 +2,16 @@
 
  const app = express(); //web app on server
 
- app.get("/user",(req, res) => {
+ app.get("/user/:userId/:name",(req, res) => {
+   console.log(req.params); //in url queryParams http://localhost:3000/user/200/Tejaswini
     res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
  });
 
- app.post("/user",(req, res) => {
-   res.send("Data saved to the DB");//response to incoming req
-});
+ app.get("/user",(req, res) => {
+   console.log(req.query); //in url queryParams http://localhost:3000/user?userId=200
+    res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
+ });
 
-app.get("/ab?c",(req, res) => { //b is optional
-   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
-});
-
-app.get("/ab+c",(req, res) => { //b can be 1 or more
-   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
-});
-
-app.get("/ab*c",(req, res) => { //anything can be between b and c
-   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
-});
-
-app.get("/a(bc)?d",(req, res) => { //bc is optional
-   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
-});
-
-app.get(/a/,(req, res) => { //if a is there anywhere
-   res.send({firstName: "Tejaswini", lastName: "Veerapur"});//response to incoming req
-});
-
-//can use regex patterns for routes
 
  app.listen(3000, ()=>{
     console.log("server is listening successfully");
