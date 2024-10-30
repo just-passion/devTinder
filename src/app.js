3e5786a9b503259f8d7e0aec837d9d7f0d795cc3
just-passion <tinycolorfulhummingbird@gmail.com>
2024-10-31 00:59:51 +0530

    changes for CORS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
