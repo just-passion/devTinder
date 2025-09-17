bb7923d15b564affb071879aac6c998a0bc19a93
just-passion <tinycolorfulhummingbird@gmail.com>
2025-09-17 17:49:05 +0530

    mongodb url changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://namastedev:NamasteDev@namastenode.esn20.mongodb.net/devTinder" //devTinder is DB name
+    "mongodb+srv://namastedev:Wini4Mongodb7@namastenode.esn20.mongodb.net/devTinder" //devTinder is DB name
   ); //returns a promise
 };
 
